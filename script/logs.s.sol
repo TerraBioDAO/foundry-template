d17a238ac05d3316e5b5b90dd0afeb51f6d22f7d
raphael <raphael.pellet@protonmail.com>
2023-02-14 10:23:01 +0100

    📄 contracts, tests and scripts templates

--- script/logs.s.sol
+++ script/logs.s.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
-
+contract logs is Script {
     function run() public {
-        vm.broadcast();
+        console.log("ChainID", block.chainid);
+
+        // call blockchain
     }
 }
